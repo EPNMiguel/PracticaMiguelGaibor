ce0ba892235197a54a9fb5cd616fd801ee1c1b5f
EPNMiguel <miguel_gaibor_mg@hotmail.com>
2023-01-14 07:02:35 -0500

    feat: se crea login

--- PracticaMiguelGaibor/Form1.Designer.cs
+++ PracticaMiguelGaibor/Form1.Designer.cs
@@ -29,11 +29,115 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnIngresar);
+            this.panel1.Controls.Add(this.tbPass);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbUser);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 463);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIngresar.Location = new System.Drawing.Point(152, 365);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(152, 65);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(124, 304);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.PasswordChar = '*';
+            this.tbPass.Size = new System.Drawing.Size(240, 27);
+            this.tbPass.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(124, 247);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(240, 27);
+            this.tbUser.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(175, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(483, 495);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Práctica Miguel Gaibor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button btnIngresar;
+        private TextBox tbPass;
+        private Label label3;
+        private TextBox tbUser;
+        private Label label2;
+        private ImageList imageList1;
     }
 }
--- PracticaMiguelGaibor/Form1.cs
+++ PracticaMiguelGaibor/Form1.cs
@@ -1,3 +1,9 @@
+
+
+
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+
 namespace PracticaMiguelGaibor
 {
     public partial class Form1 : Form
@@ -5,6 +11,38 @@ namespace PracticaMiguelGaibor
         public Form1()
         {
             InitializeComponent();
+        }
+        string conection = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\A\\source\\repos\\PracticaMiguelGaibor\\PracticaMiguelGaibor\\AppData\\Database1.mdf;Integrated Security=True";
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (tbUser.Text.Equals("") || tbPass.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese credenciales");
+                return;
+            }
+            try
+            {
+                SqlConnection conn = new SqlConnection(conection);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select nombre , apellido from persona where usuario = @usuario and password = @pass", conn);
+                cmd.Parameters.AddWithValue("@usuario", tbUser.Text);
+                cmd.Parameters.AddWithValue("@pass", tbPass.Text);
+                SqlDataReader reader = cmd.ExecuteReader(); 
+                reader.Read();  
+                if (!reader.GetString(0).IsNullOrEmpty())
+                {
+                    MessageBox.Show("Simón " + reader.GetString(0)+ " " + reader.GetString(1));
+                }
+                else
+                {
+                    MessageBox.Show("NEL");
+                }
+
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show("OOPS: " + ex.ToString()); }
         }
     }
 }
